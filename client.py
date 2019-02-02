d5d36c70240cf625b7058e847218db7350a51ac2
Steven Mercatante <steven.mercatante@gmail.com>
2019-02-02 09:56:46 -0500

    explicitly pass owner & repo args to client funcs

--- client.py
+++ client.py
@@ -8,8 +8,6 @@ from utils import increment_version
 
 
 API_ROOT = 'https://api.github.com/'
-REPO_OWNER = os.getenv('GITHUB_REPO_OWNER')
-REPO_NAME = os.getenv('GITHUB_REPO_NAME')
 
 
 def _get_auth():
@@ -27,19 +25,20 @@ def _post(url, data):
     return requests.post(full_url, auth=_get_auth(), data=data)
 
 
-def get_releases():
-    url = f'repos/{REPO_OWNER}/{REPO_NAME}/releases'
+def get_releases(owner, repo):
+    url = f'repos/{owner}/{repo}/releases'
     return _get(url)
 
 
-def get_latest_release():
+def get_latest_release(owner, repo):
     # TODO: docstring mention that this isn't the same as the repos/latest call from the API
     # TODO: possible to call API and only fetch one result?
-    return get_releases().json()[0]
+    print('!!!!!', get_releases(owner, repo).json())
+    return get_releases(owner, repo).json()[0]
 
 
-def compare_commits(base, head):
-    url = f'repos/{REPO_OWNER}/{REPO_NAME}/compare/{base}...{head}'
+def compare_commits(owner, repo, base, head):
+    url = f'repos/{owner}/{repo}/compare/{base}...{head}'
     return _get(url)
 
 
@@ -52,15 +51,15 @@ def get_merges(commits):
     return [c for c in commits if c['commit']['message'].startswith('Merge pull request')]
 
 
-def create_release(release_type):
+def create_release(owner, repo, release_type):
     print('create_release()', release_type)
     # TODO: handle case where there are no existing releases and treat the base as v0.0.0
-    latest_release_tag = get_latest_release()['tag_name']
-    # print('latest_release_tag:', latest_release_tag)
+    latest_release_tag = get_latest_release(owner, repo)['tag_name']
+    print('latest_release_tag:', latest_release_tag)
     next_tag = increment_version(latest_release_tag, release_type)
-    # print('next_tag:', next_tag)
+    print('next_tag:', next_tag)
 
-    url = f'repos/{REPO_OWNER}/{REPO_NAME}/releases'
+    url = f'repos/{owner}/{repo}/releases'
     # TODO: name should able to be passed as an arg
     # TODO: draft should able to be passed as an arg
     payload = json.dumps({
