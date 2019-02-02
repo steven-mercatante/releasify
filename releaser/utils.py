45ec8eb5f96c793a51cacf7e174215fe33e7f0ee
Steven Mercatante <steven.mercatante@gmail.com>
2019-02-02 10:29:43 -0500

    fix increment_version

--- releaser/utils.py
+++ releaser/utils.py
@@ -3,7 +3,6 @@ import re
 pattern = re.compile(r'[vV](?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)')
 
 
-# TODO: unit test
 def increment_version(version, release_type):
 	parts = pattern.match(version).groupdict()
 	major = int(parts['major'])
@@ -12,8 +11,11 @@ def increment_version(version, release_type):
 	
 	if (release_type == 'major'):
 		major += 1
+		minor = 0
+		patch = 0
 	elif (release_type == 'minor'):
 		minor += 1
+		patch = 0
 	elif (release_type == 'patch'):
 		patch += 1
 	
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,30 @@
+import pytest
+
 from releaser.utils import increment_version
 
-def test_increment_major_version():
-    assert increment_version('v0.0.0', 'major') == 'v1.0.0'
-    assert increment_version('v0.0.1', 'major') == 'v1.0.0'
-    assert increment_version('v0.1.1', 'major') == 'v1.0.0'
+
+@pytest.mark.parametrize("test_input,expected", [
+    ('v0.0.0', 'v1.0.0'),
+    ('v0.0.1', 'v1.0.0'),
+    ('v0.1.1', 'v1.0.0'),
+])
+def test_increment_major_version(test_input, expected):
+    assert increment_version(test_input, 'major') == expected
+
+
+@pytest.mark.parametrize("test_input,expected", [
+    ('v0.0.0', 'v0.1.0'),
+    ('v0.0.1', 'v0.1.0'),
+])
+def test_increment_minor_version(test_input, expected):
+    assert increment_version(test_input, 'minor') == expected
+
+
+@pytest.mark.parametrize("initial_version,expected", [
+    ('v0.0.0', 'v0.0.1'),
+    ('v0.0.1', 'v0.0.2'),
+    ('v0.1.1', 'v0.1.2'),
+    ('v1.1.1', 'v1.1.2'),
+])
+def test_increment_patch_version(initial_version, expected):
+    assert increment_version(initial_version, 'patch') == expected
