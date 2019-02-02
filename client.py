d6db1890cd7a5eb84e33bc3ccb6658fa63725634
Steven Mercatante <steven.mercatante@gmail.com>
2019-02-01 22:08:24 -0500

    get create_release() working

--- client.py
+++ client.py
@@ -1,5 +1,9 @@
+import json
 import os
 import requests
+from pprint import pprint
+
+from utils import increment_version
 
 
 API_ROOT = 'https://api.github.com/'
@@ -7,13 +11,19 @@ REPO_OWNER = os.getenv('GITHUB_REPO_OWNER')
 REPO_NAME = os.getenv('GITHUB_REPO_NAME')
 
 
+def _get_auth():
+    return os.getenv('GITHUB_USER'), os.getenv('GITHUB_PASSWORD')
+
+
 def _get(url):
     full_url = f'{API_ROOT}{url}'
+    return requests.get(full_url, auth=_get_auth())
+
+
+def _post(url, data):
+    full_url = f'{API_ROOT}{url}'
     # print('full_url:', full_url)
-    return requests.get(
-        full_url,
-        auth=(os.getenv('GITHUB_USER'), os.getenv('GITHUB_PASSWORD'))
-    ).json()
+    return requests.post(full_url, auth=_get_auth(), data=data)
 
 
 def get_releases():
@@ -22,8 +32,9 @@ def get_releases():
 
 
 def get_latest_release():
+    # TODO: docstring mention that this isn't the same as the repos/latest call from the API
     # TODO: possible to call API and only fetch one result?
-    return get_releases()[0]
+    return get_releases().json()[0]
 
 
 def compare_commits(base, head):
@@ -33,8 +44,31 @@ def compare_commits(base, head):
 
 def get_commits_since_release(release=None):
     base = release or get_latest_release()['tag_name']
-    return compare_commits(base, 'develop')
+    return compare_commits(base, 'master')
 
 
 def get_merges(commits):
     return [c for c in commits if c['commit']['message'].startswith('Merge pull request')]
+
+
+def create_release(release_type):
+    print('create_release()', release_type)
+    latest_release_tag = get_latest_release()['tag_name']
+    # print('latest_release_tag:', latest_release_tag)
+    next_tag = increment_version(latest_release_tag, release_type)
+    # print('next_tag:', next_tag)
+
+    url = f'repos/{REPO_OWNER}/{REPO_NAME}/releases'
+    # TODO: name should able to be passed as an arg
+    # TODO: draft should able to be passed as an arg
+    payload = json.dumps({
+        'tag_name': next_tag, 
+        'target_commitish': 'master',
+        'name': next_tag, 
+        'draft': False, 
+        'prerelease': True, 
+        'body': 'placeholder',
+    })
+
+    resp = _post(url, payload)
+    print('resp:', resp)
--- main.py
+++ main.py
@@ -5,16 +5,19 @@ from client import (
     get_latest_release,
     get_commits_since_release,
     get_merges,
+    create_release
 )
 
 
 def main():
-    latest_release = get_latest_release()['tag_name']
-    commits_since_latest_release = get_commits_since_release(latest_release)['commits']
-    merges = get_merges(commits_since_latest_release)
-    print(merges)
-    for m in merges:
-        print(m['commit']['message'])
+    # latest_release = get_latest_release()['tag_name']
+    # commits_since_latest_release = get_commits_since_release(latest_release)['commits']
+    # merges = get_merges(commits_since_latest_release)
+    # print(merges)
+    # for m in merges:
+    #     print(m['commit']['message'])
+
+    create_release('patch')
 
 if __name__ == '__main__':
     main()
