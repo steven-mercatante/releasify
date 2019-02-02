36818a03c3b12200ab34572c76e33f9fd867b558
Steven Mercatante <steven.mercatante@gmail.com>
2019-02-01 19:19:24 -0500

    test connecting to github API

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+
 def main():
-    print('hi!')
+    r = requests.get('https://api.github.com/user', auth=(os.getenv('GITHUB_USER'), os.getenv('GITHUB_PASSWORD')))
+    print(r.content)
 
 if __name__ == '__main__':
+    load_dotenv()
     main()
