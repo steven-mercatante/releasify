62a5881af773897460f7c7ed8437596ae059fa83
Steven Mercatante <steven.mercatante@gmail.com>
2019-02-02 12:50:14 -0500

    working on AuthMiddleware for api

--- releaser/web.py
+++ releaser/web.py
@@ -2,14 +2,28 @@ from dotenv import load_dotenv
 load_dotenv()
 
 import json
+from base64 import b64decode
 
 import falcon
 
 from .client import create_release
 
 
+class AuthMiddleware(object):
+    def process_request(self, req, resp):
+        auth = req.get_header('Authorization')
+        if auth is None:
+            raise falcon.HTTPUnauthorized('Please provide a username and password')
+
+        if not auth.startswith('Basic '):
+            raise falcon.HTTPUnauthorized('Basic auth required')
+
+        encoded_creds = auth.lstrip('Basic ')
+        user, password = b64decode(encoded_creds).decode().split(':')
+
+
 # TODO: add basic auth
-class ReleaseResource:
+class ReleaseResource(object):
     def on_post(self, req, resp):
         payload = json.load(req.bounded_stream)
 
@@ -22,5 +36,5 @@ class ReleaseResource:
         resp.media = result.status_code
 
 
-api = falcon.API()
+api = falcon.API(middleware=[AuthMiddleware()])
 api.add_route('/releases', ReleaseResource())
