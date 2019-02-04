0ac2c1fa81cb0669893f739d820124d288939154
Steven Mercatante <steven.mercatante@gmail.com>
2019-02-04 11:43:17 -0500

    add dryrun arg to cli

--- releaser/cli.py
+++ releaser/cli.py
@@ -14,15 +14,18 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('owner', help='The owner of the repo')
     parser.add_argument('repo', help='The name of the repo')
-    parser.add_argument('release_type', help='The type of release')
+    parser.add_argument('type', help='The type of release')
+    parser.add_argument('-d', '--dryrun', help='Perform a dry run (doesn\'t create the release)', 
+                        action='store_true')
     args = parser.parse_args()
 
     # TODO: let user & password be passed in via optional CLI args
     client = Client(os.getenv('GITHUB_USER'), os.getenv('GITHUB_PASSWORD'))
-    result = client.create_release(args.owner, args.repo, args.release_type)
+    result = client.create_release(args.owner, args.repo, args.type, dry_run=args.dryrun)
 
     if result['ok']:
-        print(f'Created release {result["tag_name"]}')
+        print(f'Release name: {result["tag_name"]}')
+        print(f'Release body: {result["body"]}')
     else:
         # TODO: show error code & message?
         pass
