0f72ca674b505184c5462041a218f31d85844539
Steven Mercatante <steven.mercatante@gmail.com>
2019-02-03 22:42:50 -0500

    update cli.py to use Client

--- releaser/cli.py
+++ releaser/cli.py
@@ -1,9 +1,13 @@
 import argparse
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from dotenv import load_dotenv
 load_dotenv()
 
-from client import create_release
+from releaser.client import Client
 
 
 if __name__ == '__main__':
@@ -12,5 +16,10 @@ if __name__ == '__main__':
     parser.add_argument('repo', help='The name of the repo')
     parser.add_argument('release_type', help='The type of release')
     args = parser.parse_args()
-    resp = create_release(args.owner, args.repo, args.release_type)
-    print(resp.status_code)
+
+    # TODO: let user & password be passed in via optional CLI args
+    client = Client(os.getenv('GITHUB_USER'), os.getenv('GITHUB_PASSWORD'))
+    result = client.create_release(args.owner, args.repo, args.release_type)
+
+    if result['resp'].status_code == 201:
+        print(f'Created release {result["tag_name"]}')
