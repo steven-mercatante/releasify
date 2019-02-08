21cf8b83a10fe00d83942954f34954f4314b311c
Steven Mercatante <steven.mercatante@gmail.com>
2019-02-08 09:24:29 -0500

    start working on additional tests

--- tests/test_web.py
+++ tests/test_web.py
@@ -1,9 +1,11 @@
 import base64
 import json
+from unittest.mock import MagicMock
 
 from falcon import testing
 import pytest
 
+from releasify.client import Client
 from releasify.web import (
     MissingRequiredArgError, 
     create_api, 
@@ -11,10 +13,14 @@ from releasify.web import (
 )
 
 
-
 @pytest.fixture()
-def client():
+def http_client():
     return testing.TestClient(create_api())
+
+
+def _get_headers():
+    token = base64.b64encode(b'foo:bar').decode('utf-8')
+    return {'Authorization': f'Basic {token}', 'Content-Type': 'application/json'}
 
 
 def test_get_required_arg():
@@ -29,17 +35,41 @@ def test_get_required_arg_raises():
         get_required_arg(args, 'baz')
 
 
+def test_api_success(http_client):
+    # client = Client('user', 'password')
+    # client.get_default_branch = MagicMock(return_value='master')
+    # client.get_latest_release_tag = MagicMock(return_value='v1.0.0')
+    # client.get_commits_since_release = MagicMock(return_value=[])
+
+    payload = {
+        'owner': 'foo',
+        'repo': 'bar',
+        'release_type': 'major',
+        'dry_run': True
+    }
+
+    resp = http_client.simulate_post('/releases', headers=_get_headers(), json=payload)
+
+    print(resp.status_code)
+    print(resp.json)
+    
+    assert resp.status_code == 201
+    assert resp.json['tag_name'] == 'v2.0.0'
+
+
 @pytest.mark.parametrize('payload,expected', [
     ({'_owner': 'foo', 'repo': 'bar', 'release_type': 'major'}, 'owner'),
     ({'owner': 'foo', '_repo': 'bar', 'release_type': 'major'}, 'repo'),
     ({'owner': 'foo', 'repo': 'bar', '_release_type': 'major'}, 'release_type'),
 ])
-def test_missing_required_args_raises(payload, expected, client):
-    token = base64.b64encode(b'foo:bar').decode('utf-8')
-    headers = {'Authorization': f'Basic {token}', 'Content-Type': 'application/json'}
+def test_missing_required_args_raises(payload, expected, http_client):
     expected_err_msg = f"You're missing the required `{expected}` argument"
 
-    resp = client.simulate_post('/releases', headers=headers, json=payload)
+    resp = http_client.simulate_post('/releases', headers=_get_headers(), json=payload)
 
     assert resp.status_code == 500
     assert resp.json['description'] == expected_err_msg
+
+
+def test_missing_credentials_raises():
+    raise NotImplementedError()
