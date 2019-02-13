f15d4d9600354333598492258a21522b0e51fe7b
Steven Mercatante <steven.mercatante@gmail.com>
2019-02-13 08:32:18 -0500

    ver 0.6.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="releasify",
-    version="0.5.0",
+    version="0.6.0",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/steven-mercatante/releasify",
